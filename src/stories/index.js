62afd6fe59b1a4d29a3f74aa8fe12dda9be1e0ce
Darren Ivey <darrenivey@sysadmins-MBP.localdomain>
2018-08-22 15:18:48 +0100

    Add story book

---
+++ src/stories/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+// Components
+import LocationAndDateForm from '../components/LocationAndDateForm/LocationAndDateForm';
+import SunActivity from '../components/SunActivity/SunActivity';
+
+storiesOf('Sun Activity', module)
+  .add('LocationAndDateForm', () => <LocationAndDateForm />)
+  .add('SunActivity with times props', () => <SunActivity sunrise="06:30" sunset="20:00" />)
